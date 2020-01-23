cfe6e30dcf7a853387be4f3c63ef927d7e4c6eb7
fbm <wuq_xd@163.com>
2020-01-23 15:37:09 +0800

    第一个爬虫 爬取百度首页

---
+++ firstCrawlDemo/firstCrawlDemo/spiders/baidu.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BaiduSpider(scrapy.Spider):
+    name = 'baidu'
+    allowed_domains = ['http://www.baidu.com']
+    start_urls = ['http://www.baidu.com/']
+
+    def parse(self, response):
+
+        print response.body
+        #pass
